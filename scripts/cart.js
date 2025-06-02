89ac30deacfbb82587bf9ab3ff9051d3420c4ea1
My Name <myemail@example.com>
2025-06-02 08:19:32 +0530

    added dropdown in cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -6,21 +6,29 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     const productId = button.dataset.productId;
 
     let matchingItem;
+cart.forEach((item) => {
+  if (productId === item.productId) {
+    matchingItem = item;
+  }
+});
+const quantity = Number(
+    document.querySelector(`.js-quantity-selector-${productId}`).value
+  );
+if (matchingItem) {
+  matchingItem.quantity += quantity;
+} else {
+  
+  cart.push({
+    productId: productId,
+    quantity: quantity
+  });
+}
+
+    let cartQuantity = 0;
+
     cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
+        cartQuantity += item.quantity;
     });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-
-    console.log(cart);
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
